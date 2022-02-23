bbe9a24ccf381c359b5597a98f8d714a5d78ca94
Larry Kagan <larry@superiocity.com>
2022-02-23 10:36:23 -0500

    Setup DB

---
+++ tests/Feature/TaskControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class TaskControllerTest extends TestCase
+{
+    public function test_adding_task_places_in_list()
+    {
+        $this->
+    }
+}
